5552f0a57a01098925dcf6be298064863a9f1b19
Emekcan <emekcan.aras@arm.com>
2022-09-29 15:53:54 +0100

    Add corstone1000 platform
    Adds corstone1000 platform and related
    configurations under product folder as a
    new product.
    
    Signed-off-by: Emekcan Aras <emekcan.aras@arm.com>

---
+++ product/corstone-1000/firmware/firmware.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2019, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+BS_FIRMWARE_CPU := cortex-m3
+BS_FIRMWARE_HAS_MULTITHREADING := no
+BS_FIRMWARE_HAS_NOTIFICATION := yes
+BS_FIRMWARE_HAS_OPENAMP := yes
+
+BS_FIRMWARE_MODULES := \
+    pl011 \
+    log \
+    mhuv2 \
+    eventhandler
+
+BS_FIRMWARE_SOURCES := \
+    config_log.c \
+    config_mhuv2.c \
+    config_eventhandler.c
+
+include $(BS_DIR)/firmware.mk
---
+++ product/corstone-1000/include/system_clock.h
@@ -0,0 +1,18 @@
+/*
+ *
+ * Copyright (c)  2019, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+
+#ifndef SYSTEM_CLOCK_H
+#define SYSTEM_CLOCK_H
+
+#define __XTAL            (50000000UL)    /* Oscillator frequency             */
+
+#define SYSTEM_CLOCK    (__XTAL / 2)
+
+
+#endif /* SYSTEM_CLOCK_H */
